4e045b2943432da1221288ef95bab323efd37304
PashPush <greentoryrock@gmail.com>
2024-01-26 11:01:56 +0400

    small fix

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -1,5 +1,4 @@
 import { FullCard } from '@/components/auth/FullCard'
-import { Card } from '@/components/ui/card'
 import { ExclamationTriangleIcon } from '@radix-ui/react-icons'
 import React from 'react'
 
